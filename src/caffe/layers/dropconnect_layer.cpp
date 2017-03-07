97f16b4d02476807d77e5b553c68605d52bd85d5
Gustavo Rosa <gth.rosa@uol.com.br>
2017-03-07 15:47:44 -0500

    Adding dropconnect layer. Still a long path to go.

---
+++ src/caffe/layers/dropconnect_layer.cpp
@@ -0,0 +1,102 @@
+#include <vector>
+
+#include "caffe/filler.hpp"
+#include "caffe/layers/dropconnect_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void DropconnectLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  const int num_output = this->layer_param_.dropconnect_param().num_output();
+  transpose_ = false;
+  N_ = num_output;
+  const int axis = bottom[0]->CanonicalAxisIndex(1);
+  K_ = bottom[0]->count(axis);
+  threshold_ = this->layer_param_.dropconnect_param().dropconnect_ratio();
+  DCHECK(threshold_ > 0.);
+  DCHECK(threshold_ < 1.);
+  scale_ = 1. / (1. - threshold_);
+  uint_thres_ = static_cast<unsigned int>(UINT_MAX * threshold_);
+  LOG(INFO) << "Dropconnect ratio: " << threshold_ << " " << scale_ << " " << uint_thres_;
+  // Check if we need to set up the weights
+  if (this->blobs_.size() > 0) {
+    LOG(INFO) << "Skipping parameter initialization";
+  } else {
+    this->blobs_.resize(1);
+    // Initialize the weights
+    vector<int> weight_shape(2);
+    weight_shape[0] = N_;
+    weight_shape[1] = K_;
+    this->blobs_[0].reset(new Blob<Dtype>(weight_shape));
+    // fill the weights
+    shared_ptr<Filler<Dtype> > weight_filler(GetFiller<Dtype>(
+        this->layer_param_.dropconnect_param().weight_filler()));
+    weight_filler->Fill(this->blobs_[0].get());
+  }  // parameter initialization
+  this->param_propagate_down_.resize(this->blobs_.size(), true);
+  LOG(INFO) << "Dropconnect Setup OK";
+}
+
+template <typename Dtype>
+void DropconnectLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  // Figure out the dimensions
+  const int axis = bottom[0]->CanonicalAxisIndex(1);
+  const int new_K = bottom[0]->count(axis);
+  CHECK_EQ(K_, new_K)
+      << "Input size incompatible with inner product parameters.";
+  // The first "axis" dimensions are independent inner products; the total
+  // number of these is M_, the product over these dimensions.
+  M_ = bottom[0]->count(0, axis);
+  // The top shape will be the bottom shape with the flattened axes dropped,
+  // and replaced by a single axis with dimension num_output (N_).
+  vector<int> top_shape = bottom[0]->shape();
+  top_shape.resize(axis + 1);
+  top_shape[axis] = N_;
+  top[0]->Reshape(top_shape);
+}
+
+template <typename Dtype>
+void DropconnectLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  Dtype* top_data = top[0]->mutable_cpu_data();
+  const Dtype* weight = this->blobs_[0]->cpu_data();
+  caffe_cpu_gemm<Dtype>(CblasNoTrans, transpose_ ? CblasNoTrans : CblasTrans,
+      M_, N_, K_, (Dtype)1.,
+      bottom_data, weight, (Dtype)0., top_data);
+}
+
+template <typename Dtype>
+void DropconnectLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down,
+    const vector<Blob<Dtype>*>& bottom) {
+  if (this->param_propagate_down_[0]) {
+    const Dtype* top_diff = top[0]->cpu_diff();
+    const Dtype* bottom_data = bottom[0]->cpu_data();
+    // Gradient with respect to weight
+      caffe_cpu_gemm<Dtype>(CblasTrans, CblasNoTrans,
+          N_, K_, M_,
+          (Dtype)1., top_diff, bottom_data,
+          (Dtype)1., this->blobs_[0]->mutable_cpu_diff());
+  }
+  if (propagate_down[0]) {
+    const Dtype* top_diff = top[0]->cpu_diff();
+    // Gradient with respect to bottom data
+      caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans,
+          M_, K_, N_,
+          (Dtype)1., top_diff, this->blobs_[0]->cpu_data(),
+          (Dtype)0., bottom[0]->mutable_cpu_diff());
+  }
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(DropconnectLayer);
+#endif
+
+INSTANTIATE_CLASS(DropconnectLayer);
+REGISTER_LAYER_CLASS(Dropconnect);
+
+}  // namespace caffe
